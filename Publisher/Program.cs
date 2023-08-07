cdd12fd36ab26ffe102a2308a308ef00f7d03a92
Cesar <8007837+cesaroll@users.noreply.github.com>
2023-08-07 16:03:51 +0200

    Basic GCP pub/sub with c#

---
+++ Publisher/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using Google.Cloud.PubSub.V1;
+using Google.Protobuf;
+
+const string ProjectId = "development-395212";
+const string TopicId = "MyQueue";
+const string SubscriptionId = "MyQueue-sub";
+
+var topicName = new TopicName(ProjectId, TopicId);
+
+
+Console.WriteLine($"Hello {topicName.TopicId}");
+
+Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS",
+    Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "gcp_credentials.json"));
+
+
+var publisher = await PublisherClient.CreateAsync(topicName);
+
+var pubSubMessage = new PubsubMessage()
+{
+    Data = ByteString.CopyFromUtf8("This is another sample message!"),
+};
+
+string messageId = await publisher.PublishAsync(pubSubMessage);
+
+Console.WriteLine($"MessageId: {messageId}");
+
---
+++ Subscriber/Program.cs
@@ -0,0 +1,38 @@
+﻿using Google.Cloud.PubSub.V1;
+using Google.Protobuf;
+using System;
+using System.Threading.Tasks;
+using Grpc.Core;
+
+namespace PubSubConsumer
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string projectId = "development-395212";
+            string subscriptionId = "MyQueue-sub";
+
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS",
+                Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "gcp_credentials.json"));
+
+            var subscriptionName = SubscriptionName.FromProjectSubscription(projectId, subscriptionId);
+            var subscriber = await SubscriberClient.CreateAsync(subscriptionName);
+
+            int messageCount = 0;
+
+            Task startTask = subscriber.StartAsync((PubsubMessage message, CancellationToken cancel) =>
+            {
+                string text = System.Text.Encoding.UTF8.GetString(message.Data.ToArray());
+                Console.WriteLine($"Message: {message.MessageId}: {text}");
+                Interlocked.Increment(ref messageCount);
+                return Task.FromResult(SubscriberClient.Reply.Ack);
+            });
+
+            Console.WriteLine("Listening for messages. Press any key to exit...");
+            Console.ReadKey();
+
+            await subscriber.StopAsync(CancellationToken.None);
+        }
+    }
+}
